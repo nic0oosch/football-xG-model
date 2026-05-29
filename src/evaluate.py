5f85e21f82cd914ecf3e0442638534317860af66
nic0oosch <nico.schuessler@student.uni-tuebingen.de>
2026-05-29 19:24:37 +0200

    adapt evaluate and notebook to use testset

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,6 +2,10 @@ from sklearn.metrics import roc_auc_score, brier_score_loss
 
 def evaluate(model, X_test, y_test):
     y_pred = model.predict_proba(X_test)[:, 1]
-    print(f"AUC: {roc_auc_score(y_test, y_pred):.3f}")
-    print(f"Brier: {brier_score_loss(y_test, y_pred):.3f}")
+    auc = roc_auc_score(y_test, y_pred)
+    brier = brier_score_loss(y_test, y_pred)
+    
+    print(f"{auc:>6.3f} \n{brier:>7.4f}")
+
+
 
