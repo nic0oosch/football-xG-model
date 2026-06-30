b0a7398e9aeb58555bc1d6604d7d12eca1c30e64
nic0oosch <nico.schuessler@student.uni-tuebingen.de>
2026-06-30 18:22:47 +0200

    adapt requirements to use torch

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .base import make_preprocessor, make_X_tabular, make_X_heatmap, make_X_full
 from .baseline import train_baseline
 from .xgboost import train_xgboost
+from .cnn import XGNet, train_cnn
