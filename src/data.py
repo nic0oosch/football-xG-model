ae90be0b1f7df9d2e445cf90c1124a54a83aa6fc
nic0oosch <nico.schuessler@student.uni-tuebingen.de>
2026-07-18 21:09:35 +0200

    Refactor data and features, to allow more competitions

--- src/data.py
+++ src/data.py
@@ -5,6 +5,13 @@ import numpy as np
 from statsbombpy import sb
 from features import build_feature_df
 import hashlib, json
+import logging, sys
+import requests
+import re
+
+# logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 # custom typing
 type SEASON_ID = int
@@ -19,25 +26,43 @@ def _competition_hash(competitions) -> str:
     key = sorted(competitions)
     return hashlib.md5(json.dumps(key).encode()).hexdigest()[:8]
 
+def get_competitions_from_url(url, min_year=2010, gender="male"):
+    logger.debug(f"Trying to load competitions from given url: {url}")
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # parse from answer
+    competitions = response.json()
+    valid_comps = set()
+
+    
+    for comp in competitions:
+        # filter gender
+        if comp.get('competition_gender') != gender:
+            continue
+        
+        # extract year
+        season_name = comp.get('season_name', '')
+        matched_year = re.search(r'\d{4}', season_name)
+
+        if matched_year:
+            start_year = int(matched_year.group())
+            if start_year < min_year:
+                continue
+        else:
+            # no year found
+            continue
+       
+        # add competitions to valid comp list
+        valid_comps.add((comp['competition_id'], comp['season_id']))
+        logger.debug(f"Parsed competition: {comp['competition_name']} | Season: {comp['season_name']}")
+    
+    return valid_comps
+
 
 # Constants
-COMPETITIONS = {
-    # a competition is a tuple of (competition_id, season_id)
-    (55, 282),  # EURO 2024
-    (55, 43),   # EURO 2020
-    (9, 281),   # 1. Bundesliga 2023/2024
-    (9, 27),    # 1. Bundesliga 2015/2016
-    (11, 1),    # La Liga 2017/2018
-    (11, 4),    # La Liga 2018/2019
-    (11, 42),   # La Liga 2019/2020
-    (11, 90),   # La Liga 2020/2021
-    (1267, 107), # African Cup of Nations 2023
-    (16, 2),    # Champions League 2016/2017
-    (16, 1),    # Champions League 2017/2018
-    (16, 4),    # Champions League 2018/2019
-    (43, 3),    # FIFA World Cup 2018
-    (43, 106),  # FIFA World Cup 2022
-}
+STATSBOMB_OPEN_COMPETITIONS_URL = "https://raw.githubusercontent.com/hudl/open-data/refs/heads/master/data/competitions.json"
+COMPETITIONS = get_competitions_from_url(url=STATSBOMB_OPEN_COMPETITIONS_URL, min_year=2010, gender='male')
 
 RAW_CACHE = f"data/processed/shots_raw_{_competition_hash(COMPETITIONS)}.parquet"
 FEAT_CACHE = "data/processed/shots_features.parquet"
@@ -46,6 +71,7 @@ HEATMAP_CACHE = "data/processed/heatmaps.npy"
 
 # load raw data
 def load_raw_shots(competitions: set[tuple[COMPETITION_ID, SEASON_ID]] = COMPETITIONS) -> pd.DataFrame:
+    logger.info(f"Found {len(competitions)} valid competitions.")
     if os.path.exists(RAW_CACHE):
         print("Loading existing data from cache...")
         return pd.read_parquet(RAW_CACHE)
